8cd80eadb45cab2001ee969f32e606baece8299d
Berk Can Güney <berkcanguney@gmail.com>
2022-06-03 21:10:59 +0300

    Move small methods to util class

--- src/main/java/com/fleetmanagement/shipping/util/BagValidation.java
+++ src/main/java/com/fleetmanagement/shipping/util/BagValidation.java
@@ -2,16 +2,23 @@ package com.fleetmanagement.shipping.util;
 
 import org.springframework.util.ObjectUtils;
 
+import com.fleetmanagement.shipping.constant.CommonConstants;
 import com.fleetmanagement.shipping.constant.ErrorConstants;
 import com.fleetmanagement.shipping.exception.BusinessException;
 
 public class BagValidation {
 	
-	public static void isValid(String value) {
-		if (ObjectUtils.isEmpty(value) || !value.startsWith("C") || value.length() != 7) {
+	public static void checkValid(String value) {
+		if (ObjectUtils.isEmpty(value) || !value.startsWith(CommonConstants.BAG_FIRST_CHR) || value.length() != 7) {
 			throw new BusinessException(ErrorConstants.BAG_BARCODE_FORMAT_INVALID);
 		}
-		
+	}
+	
+	public static boolean isValid(String value) {
+		if (!ObjectUtils.isEmpty(value) && value.startsWith(CommonConstants.BAG_FIRST_CHR) && value.length() == 7) {
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/main/java/com/fleetmanagement/shipping/util/PackageValidation.java
+++ src/main/java/com/fleetmanagement/shipping/util/PackageValidation.java
@@ -2,24 +2,31 @@ package com.fleetmanagement.shipping.util;
 
 import org.springframework.util.ObjectUtils;
 
+import com.fleetmanagement.shipping.constant.CommonConstants;
 import com.fleetmanagement.shipping.constant.ErrorConstants;
 import com.fleetmanagement.shipping.exception.BusinessException;
 import com.fleetmanagement.shipping.model.Bag;
 import com.fleetmanagement.shipping.model.Package;
 
 public class PackageValidation {
-	
+
 	public static void haveSameDeliveryPoint(Package mPackage, Bag mBag) {
 		if (!mPackage.getDeliveryPoint().getId().equals(mBag.getDeliveryPoint().getId())) {
 			throw new BusinessException(ErrorConstants.DELIVERY_POINT_DISMATCH);
 		}
 	}
-	
-	public static void isValid(String value) {
-		if (ObjectUtils.isEmpty(value) || !value.startsWith("P") || value.length() != 11) {
+
+	public static void checkValid(String value) {
+		if (ObjectUtils.isEmpty(value) || !value.startsWith(CommonConstants.PACKAGE_FIRST_CHR) || value.length() != 11) {
 			throw new BusinessException(ErrorConstants.PACKAGE_BARCODE_FORMAT_INVALID);
 		}
-		
+	}
+
+	public static boolean isValid(String value) {
+		if (!ObjectUtils.isEmpty(value) && value.startsWith(CommonConstants.PACKAGE_FIRST_CHR) && value.length() == 11) {
+			return true;
+		}
+		return false;
 	}
 
 }
